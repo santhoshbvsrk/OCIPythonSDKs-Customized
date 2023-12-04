c546364a3fcfc9840159b7f21c214ac1706adaf2
santhoshbvsrk <53339401+santhoshbvsrk@users.noreply.github.com>
2023-12-04 21:46:20 +0530

    Create InputData_VM.py

---
+++ InputData_VM.py
@@ -0,0 +1,12 @@
+#initializing variables
+subnet_id="ocid1.subnet.oc1."
+boot_volume_id="ocid1.bootvolume.oc1."
+availability_domain="VPLM:US-ASHBURN-AD-1"
+compartment_id="ocid1.compartment.oc1."
+display_name="OCI_DR_VM"
+image_id="ocid1.image.oc1."
+shape="VM.Standard3.Flex"
+ocpus=1
+memory_in_gbs=16
+volume_id = "ocid1.volume.oc1."
+type = "iscsi"
