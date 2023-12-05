cd6ed01442183a472ca6893281d52bc0cc2d318a
santhoshbvsrk <53339401+santhoshbvsrk@users.noreply.github.com>
2023-12-05 15:31:55 +0530

    Update InputData_VM.py
    Updated fields as required

--- InputData_VM.py
+++ InputData_VM.py
@@ -1,12 +1,27 @@
 #initializing variables
-subnet_id="ocid1.subnet.oc1."
-boot_volume_id="ocid1.bootvolume.oc1."
-availability_domain="VPLM:US-ASHBURN-AD-1"
-compartment_id="ocid1.compartment.oc1."
-display_name="OCI_DR_VM"
-image_id="ocid1.image.oc1."
-shape="VM.Standard3.Flex"
-ocpus=1
-memory_in_gbs=16
-volume_id = "ocid1.volume.oc1."
-type = "iscsi"
+#Generic Fields
+subnet_id="ocid1.subnet."
+availability_domain="VPLM:AP-MUMBAI-1-AD-1"
+compartment_id="ocid1.compartment."
+is_auto_tune_enabled = True
+
+#Boot Volume related fields
+boot_vol_replica_id = "ocid1.bootvolumereplica."
+create_boot_type = "bootVolumeReplica"
+boot_vol_name = "<BootVolumeName>"
+boot_vol_size_in_gbs = <BootVolumeSizeInGB>
+boot_vol_vpus_per_gb = <VolumePerformanceUnitsInGB>
+
+#Block Volume related fields
+block_vol_replica_id = "ocid1.blockvolumereplica."
+block_vol_attachment_type = "iscsi"
+block_vol_name = "<BlockVolumeName>"
+blockvol_vpus_per_gb = <BlockVolumeSizeInGB>
+blockvol_size_in_gbs = <VolumePerformanceUnitsInGB>
+
+#Compute related fields
+vm_display_name="<Compute/VM/Instance Name>"
+vm_shape="<Compute/VM/Instance Shape>"
+#if creating VM with Flex shapes, please provide the details of OCPUs & Memory
+vm_ocpus=<ComputeOCPUs>
+vm_memory_in_gbs=<ComputeMemory>
