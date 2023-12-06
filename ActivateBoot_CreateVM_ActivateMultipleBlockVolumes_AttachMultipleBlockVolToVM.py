4d98adf571504f58f0873da38d100727057b3f21
santhoshbvsrk <53339401+santhoshbvsrk@users.noreply.github.com>
2023-12-06 18:21:27 +0530

    Update ActivateBoot_CreateVM_ActivateMultipleBlockVolumes_AttachMultipleBlockVolToVM.py
    This script will perform the below actions:
    1. Create BootVolume from BootVolumeReplica.
    2. Create VM from BootVolume created in above step.
    3. Create Multiple BlockVolumes from Multiple BlockVolumeReplicas.
    4. Attach all the created BlockVolumes to VM created in Step2.

--- ActivateBoot_CreateVM_ActivateMultipleBlockVolumes_AttachMultipleBlockVolToVM.py
+++ ActivateBoot_CreateVM_ActivateMultipleBlockVolumes_AttachMultipleBlockVolToVM.py
@@ -1,5 +1,5 @@
 # Working to create BootVolume from BootVolumeReplicas
-import InputData_VM
+import InputData_Block_Boot_VM
 import time
 import oci
 from oci.config import from_file
@@ -16,19 +16,19 @@ core_client = BlockstorageClient(config)
 # Send the request to service, some parameters are not required, see API
 create_boot_volume_response = core_client.create_boot_volume(
     create_boot_volume_details=CreateBootVolumeDetails(
-        compartment_id=InputData_VM.compartment_id,
+        compartment_id=InputData_Block_Boot_VM.compartment_id,
         source_details=BootVolumeSourceFromBootVolumeDetails(
-            id = InputData_VM.boot_vol_replica_id,
-            type= InputData_VM.create_boot_type),
-        availability_domain=InputData_VM.availability_domain,
-        display_name=InputData_VM.boot_vol_name,
-        size_in_gbs=InputData_VM.boot_vol_size_in_gbs,
-        vpus_per_gb=InputData_VM.boot_vol_vpus_per_gb,
-        is_auto_tune_enabled=InputData_VM.is_auto_tune_enabled,
+            id = InputData_Block_Boot_VM.boot_vol_replica_id,
+            type= InputData_Block_Boot_VM.create_boot_type),
+        availability_domain=InputData_Block_Boot_VM.availability_domain,
+        display_name=InputData_Block_Boot_VM.boot_vol_name,
+        size_in_gbs=InputData_Block_Boot_VM.boot_vol_size_in_gbs,
+        vpus_per_gb=InputData_Block_Boot_VM.boot_vol_vpus_per_gb,
+        is_auto_tune_enabled=InputData_Block_Boot_VM.is_auto_tune_enabled,
         boot_volume_replicas=[
             oci.core.models.BootVolumeReplicaDetails(
-                availability_domain=InputData_VM.bootvolreplica_availability_domain,
-                display_name=InputData_VM.bootvolreplica_name)]
+                availability_domain=InputData_Block_Boot_VM.bootvolreplica_availability_domain,
+                display_name=InputData_Block_Boot_VM.bootvolreplica_name)]
         )
 )
 
@@ -51,21 +51,21 @@ print('Boot Volume is created from BootVolumeReplica with OCID:',get_boot_respon
 compute_client = ComputeClient(config)
 
 # Create VNIC Details
-vnic_details = CreateVnicDetails(subnet_id=InputData_VM.subnet_id, assign_public_ip=InputData_VM.vm_assign_public_ip,private_ip=InputData_VM.vm_private_ip)
+vnic_details = CreateVnicDetails(subnet_id=InputData_Block_Boot_VM.subnet_id, assign_public_ip=InputData_Block_Boot_VM.vm_assign_public_ip,private_ip=InputData_Block_Boot_VM.vm_private_ip)
 
 # Boot Volume Details
 boot_volume_details = InstanceSourceViaBootVolumeDetails(boot_volume_id=create_bootvol_response.id)
 
 # Create Instance Details
 instance_details = LaunchInstanceDetails(
-    availability_domain=InputData_VM.availability_domain,
-    compartment_id=InputData_VM.compartment_id,
-    display_name=InputData_VM.vm_display_name,
+    availability_domain=InputData_Block_Boot_VM.availability_domain,
+    compartment_id=InputData_Block_Boot_VM.compartment_id,
+    display_name=InputData_Block_Boot_VM.vm_display_name,
     image_id=get_boot_response.image_id,
-    shape=InputData_VM.vm_shape,
+    shape=InputData_Block_Boot_VM.vm_shape,
     shape_config=LaunchInstanceShapeConfigDetails(
-            ocpus=InputData_VM.vm_ocpus,
-            memory_in_gbs=InputData_VM.vm_memory_in_gbs),
+            ocpus=InputData_Block_Boot_VM.vm_ocpus,
+            memory_in_gbs=InputData_Block_Boot_VM.vm_memory_in_gbs),
     create_vnic_details=vnic_details,
     source_details=boot_volume_details
 )
@@ -88,23 +88,23 @@ print('VM is created successfully from Boot Volume with OCID:',getinst_data.id,'
 # Initialize service client with default config file
 core_client = BlockstorageClient(config)
 
-blkvolocid = InputData_VM.block_vol_replica_ocid
+blkvolocid = InputData_Block_Boot_VM.block_vol_replica_ocid
 
 for i in range(len(blkvolocid['block_vol_replica_id'])):
     create_volume_response = core_client.create_volume(
         create_volume_details=CreateVolumeDetails(
-        compartment_id=InputData_VM.compartment_id,
-        availability_domain=InputData_VM.availability_domain,
+        compartment_id=InputData_Block_Boot_VM.compartment_id,
+        availability_domain=InputData_Block_Boot_VM.availability_domain,
         display_name=blkvolocid['block_vol_name'][i],
         vpus_per_gb=blkvolocid['blockvol_vpus_per_gb'][i],
         size_in_gbs=blkvolocid['blockvol_size_in_gbs'][i],
         source_details=oci.core.models.VolumeSourceFromBlockVolumeReplicaDetails(
             type="blockVolumeReplica",
             id=blkvolocid['block_vol_replica_id'][i]),
-        is_auto_tune_enabled=InputData_VM.is_auto_tune_enabled,
+        is_auto_tune_enabled=InputData_Block_Boot_VM.is_auto_tune_enabled,
         block_volume_replicas=[
             BlockVolumeReplicaDetails(
-                availability_domain=InputData_VM.blockvolreplica_availability_domain,
+                availability_domain=InputData_Block_Boot_VM.blockvolreplica_availability_domain,
                 display_name=blkvolocid['blockvolreplica_name'][i])]
                 )
     )
@@ -124,7 +124,7 @@ for i in range(len(blkvolocid['block_vol_replica_id'])):
 
     attach_volume_response = core_client1.attach_volume(
         attach_volume_details=AttachVolumeDetails(
-            type=InputData_VM.block_vol_attachment_type,
+            type=InputData_Block_Boot_VM.block_vol_attachment_type,
             instance_id=getinst_data.id,
             volume_id=get_blockvol_data.id))
     print('Attached Block Volume to VM')
